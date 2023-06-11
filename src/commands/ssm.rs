7327caa0bd1c40ce7ee54cb8db91a84f424a5548
John Hawksley <john_hawksley@intergral.com>
2023-06-11 16:02:20 +0200

    Update signal handling to include ctrl-z.
    - Remove ctrlc requirement
    - Use signal_hook for both
    
    jhawksley/rjaws#4

--- src/commands/ssm.rs
+++ src/commands/ssm.rs
@@ -1,6 +1,8 @@
+use std::thread;
 use std::time::Instant;
 use async_trait::async_trait;
 use subprocess::PopenConfig;
+use signal_hook::{consts::SIGTSTP, consts::SIGINT, iterator::Signals};
 
 use crate::commands::{Command, notify, to_hms};
 use crate::errors::jaws_error::JawsError;
@@ -15,6 +17,18 @@ impl SSMCommand {
         Self {
             instance_id: instance_id.to_string()
         }
+    }
+
+    /// Set up ctrl-C and ctrl-Z signal handlers so they are passed to the subprocess
+    fn set_signal_handlers() {
+        let mut signals = Signals::new(&[SIGTSTP, SIGINT]).expect("Couldn't create Signals instance.");
+
+        thread::spawn(move || {
+            for _sig in signals.forever() {
+                // println!("Received signal {:?}", sig);
+                // We don't need to do anything.
+            }
+        });
     }
 }
 
@@ -31,18 +45,12 @@ impl Command for SSMCommand {
         // switches to a binary format, described here:
         // https://github.com/aws/amazon-ssm-agent/blob/c65d8ac29a8bbe6cd3f7cea778c1eeb1b06d49a3/agent/session/contracts/agentmessage.go
 
-        // To get something running, we use the old Jaws 2 way of spawning SSM - spwan the AWS
+        // To get something running, we use the old Jaws 2 way of spawning SSM - spawn the AWS
         // SSM module.
 
         let cmd_string = &["aws", "ssm", "start-session", "--target", &self.instance_id];
+        Self::set_signal_handlers();
         let popen_res = subprocess::Popen::create(cmd_string, PopenConfig::default());
-
-        // Signal handler
-
-        ctrlc::set_handler(move || {
-            // We don't need to do anything in here.  It will go to the subprocess.
-        }).expect("Error setting Ctrl-C handler");
-
         popen_res.expect("Couldn't open the AWS SSM module, ensure it is installed.");
 
         // Session is complete here
